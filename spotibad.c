6b2c41be11fb8c47fb9fa4709fe15d8e25ca27ed
Othman Bichouna <obichouna@stuy.edu>
2017-10-17 11:47:45 -0400

    Lets go boys

---
+++ spotibad.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "spotibad.h"
+#include "linked_list.h"
+
+int main()
+
+  printf("___ ___  ___ _____ ___ ___   _   ___  
+ / __| _ \/ _ \_   _|_ _| _ ) /_\ |   \ \n 
+ \__ \  _/ (_) || |  | || _ \/ _ \| |) | \n
+ |___/_|  \___/ |_| |___|___/_/ \_\___/ \n");
