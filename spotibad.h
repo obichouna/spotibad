e2e09625d62401aa772afe0f6134c93b3744aad5
Othman Bichouna <obichouna@stuy.edu>
2017-10-18 11:35:13 -0400

    Fixed headers

--- spotibad.h
+++ spotibad.h
@@ -1,8 +1,9 @@
-void * add_song_node(struct song_node);
+void add_song_node(struct song_node);
 struct song_node * song_search(char[] song, char[] artist);
 struct song_node * artist_search(char[] artist_name);
 void print_letter(char directory);
-void print_letter_songs(char directory);
+void print_artists_songs(char[] artist);
+void print_library();
 void shuffle();
 struct song_node * delete_song(struct song_node *song);
-void * clear_all_songs();
+void * clear_all();
