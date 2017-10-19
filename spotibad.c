1772e2d817da45aefa8bf0fed179bff76798ff23
Othman Bichouna <obichouna@stuy.edu>
2017-10-18 23:11:40 -0400

    Going to work on linked_list functionality first

--- spotibad.c
+++ spotibad.c
@@ -17,8 +17,21 @@ int main(){
 
 void add_song_node(struct song_node track){
   char first_letter = track.artist[0];
-  first_letter + 32;
+  first_letter = first_letter + 32;
   int i = first_letter - 97;
   
+  
+}
+
+struct song_node * song_search(char[] song, char[] artist){
+  int i = artist[0];
+  
+
+
+
+}
+
+struct song_node * artist_search (char[] artist_name){
+  int i = artist[0];
 
 }
