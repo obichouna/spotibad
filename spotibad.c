1b5dd956bb6fecd2c9ea122892c741acfebbcbd9
Jeffrey Lin <jeffrey@icurse.nl>
2017-10-19 21:57:00 -0400

    Move main() {spotibad -> tests}.c

--- spotibad.c
+++ spotibad.c
@@ -3,18 +3,6 @@
 #include "spotibad.h"
 #include "linked_list.h"
 
-int main(){
-
-  printf("  ___ ___  ___ _____ ___ ___   _   ___  \n");
-  printf(" / __| _ \\/ _ \\_   _|_ _| _ ) /_\\ |   \\ \n");
-  printf(" \\__ \\  _/ (_) || |  | || _ \\/ _ \\| |) |\n");
-  printf(" |___/_|  \\___/ |_| |___|___/_/ \\_\\___/ \n");
-  printf("                                        \n");
-  printf(" ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ \n");
-
- }
-
-
 void add_song_node(struct song_node track){
   char first_letter = track.artist[0];
   int i = first_letter - 65; // Getting the index of the alphabet[] array
