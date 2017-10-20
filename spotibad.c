3508105b0fda343682a505306d7434ec0d819a78
Jeffrey Lin <jeffrey@icurse.nl>
2017-10-19 22:04:34 -0400

    spotibad.c: fix glaring errors

--- spotibad.c
+++ spotibad.c
@@ -1,25 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "spotibad.h"
-#include "linked_list.h"
+
+struct song_node *table[27];
 
 void add_song_node(struct song_node track){
   char first_letter = track.artist[0];
   int i = first_letter - 65; // Getting the index of the alphabet[] array
-  
-  
+
+
 }
 
-struct song_node * song_search(char[] song, char[] artist){
+struct song_node * song_search(char song[], char artist[]) {
   int i = artist[0];
   i = i - 65; // Getting the index of the alphabet[] array
-  
+
 
 
 
 }
 
-struct song_node * artist_search (char[] artist_name){
+struct song_node * artist_search (char artist[]){
   int i = artist[0];
   i = i - 65; // Getting the index of the alphabet[] array
 }
--- spotibad.h
+++ spotibad.h
@@ -1,8 +1,10 @@
+#include "linked_list.h"
+
 void add_song_node(struct song_node);
-struct song_node * song_search(char[] song, char[] artist);
-struct song_node * artist_search(char[] artist_name);
+struct song_node * song_search(char song[], char artist[]);
+struct song_node * artist_search(char artist_name[]);
 void print_letter(char directory);
-void print_artists_songs(char[] artist);
+void print_artists_songs(char artist[]);
 void print_library();
 void shuffle();
 struct song_node * delete_song(struct song_node *song);
