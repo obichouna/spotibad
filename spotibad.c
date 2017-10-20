9089750d21af1d1e3984ed90021ce94957a41edf
Othman Bichouna <obichouna@stuy.edu>
2017-10-19 23:48:18 -0400

    Made song_search

--- spotibad.c
+++ spotibad.c
@@ -17,10 +17,20 @@ void add_song_node(struct song_node track){
 struct song_node * song_search(char song[], char artist[]) {
   int i = artist[0];
   i = i - 65; // Getting the index of the alphabet[] array
+  struct song_node* head = table[i];
+  int tmp;
+  int tmp2;
+  while(head){
+    tmp = strcmp(head->name, song);
+    tmp2 = strcmp(head->artist, artist);
+    if (tmp == 0 && tmp2 == 0){
+      return head;
+    }
+    head = head->next;
 
-
-
-
+  }
+  printf("Song not found");
+  return head;
 }
 
 struct song_node * artist_search (char artist[]){
